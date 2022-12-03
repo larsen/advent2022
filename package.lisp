2a3a2b772c055c57723b5544d9c2091891b6702a
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2022-12-03 22:50:38 +0100

    Day 2 & 3

--- package.lisp
+++ package.lisp
@@ -1,6 +1,12 @@
 ;;;; package.lisp
 
 (defpackage #:advent2022
-  (:use #:cl #:split-sequence)
+  (:use #:cl #:split-sequence #:cl-ppcre)
   (:export day1/solution1
-           day1/solution2))
+           day1/solution2
+
+           day2/solution1
+           day2/solution2
+
+           day3/solution1
+           day3/solution2))
--- tests/main.lisp
+++ tests/main.lisp
@@ -3,3 +3,11 @@
 (define-test day1
   (is = (day1/solution1) 69693)
   (is = (day1/solution2) 200945))
+
+;; (define-test day2
+;;   (is = (day2/solution1) 11386)
+;;   (is = (day2/solution2) 13600))
+
+(define-test day3
+  (is = (day3/solution1) 7817)
+  (is = (day3/solution2) 2444))
